c4326a733a116d0d7cfbf1c853c389cf0c7a5372
GregoryMatesi <44989448+GregoryMatesi@users.noreply.github.com>
2019-03-19 10:03:01 -0600

    Update SLSQPmixturesR.R

--- SLSQPmixturesR.R
+++ SLSQPmixturesR.R
@@ -6,7 +6,7 @@
 
 
 
-mixtures <- function(A, pop){
+SLSQPmixtures <- function(A, pop){
     master_frame_final <- A # user enters a matrix
     pop_number <- pop       # user defined population number
 
@@ -47,8 +47,8 @@ mixtures <- function(A, pop){
     start_time = Sys.time()
     S <- slsqp(starting, fn = fn.ancmixtures, hin = hin.ancmixtures, heq = heq.ancmixtures)
     end_time = Sys.time()
-    returnThese <- list(S$par, S$iter, end_time - start_time)  # pi estimates, # of iterations, and time. To be returned.
-    returnThese   # return 3 things.
+    returnThese <- list(S$par[1], S$par[2], S$par[3], S$par[4], S$par[5], S$iter, end_time - start_time)  # pi estimates, # of iterations, and time. To be returned.
+    returnThese   # return 7 things.
     #S$par
     #end_time - start_time
 
