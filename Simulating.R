0f8a95ce9c9122d0b59f8bf14b4528bacf9185bd
GregoryMatesi <44989448+GregoryMatesi@users.noreply.github.com>
2019-03-20 13:34:51 -0600

    Update Simulating.R

--- Simulating.R
+++ Simulating.R
@@ -1,10 +1,10 @@
 # "snpsampgen.R" contains a function that takes a new set of 100,000 SNP and uses those to simulate a sample population of size k.
 
-# "SLSQPmixturesR.R" contains a function that feeds this population matrix into the SLSQP algorithm in R.
+# "SLSQPmixturesR.R" contains a function that feeds this sample population matrix into the SLSQP algorithm in R.
 
 # "HA_script.py" contains a function that feeds this sample population matrix into the SLSQP algorithm in Python.
 
-# At the end of every iteration of this look will be printed the estimated pi-values, number of iterations, and time for both versions of SLSQP.
+# At the end of every iteration of this loop will be printed the estimated pi-values, number of iterations, and time for both versions of SLSQP.
 
 # The source scripts needed to execute this code can be found at https://github.com/GregoryMatesi/SimulationDesign
 
@@ -14,45 +14,53 @@
 
 # Thanks to Ian for "snpsampgen.R" and "SLSQPmixturesR.R." And to Jordan for "HA_script.py."
 
-#load(file="mixtures/Mixtures.git/total_strict_fine_maf01_atleastone.RData")
-install.packages("nloptr")    # Needs to be installed on the server. IT contains our SLSQP R function.
+total.c <- read.table("/home/jovyan/resources/5ancfinalsimdat.txt")
+#install.packages("nloptr")    # Needs to be installed on the server. It contains our SLSQP R function.
 library("nloptr")
 library("reticulate")         # Reticulate is already installed on the server. It links Python with R.
 
-rm(snpsampgen, SLSQPmixtures, HA, R, minimize, r, guess, af, A, i, numberSims, population1, population2, guess1, guess2, k, popfrac1, popfrac2, END, START, HA_, Python_) # remove everything except for total.c because it is a huge file.
-
 source("/home/jovyan/work/snpsampgen.R")
 source("/home/jovyan/work/SLSQPmixturesR.R")
-source_python("/home/jovyan/work/HA_script.py")    # From the "reticulate" package.
+source_python("/home/jovyan/work/HA_script.py")    # source_Python() from the "reticulate" package.
 
 START <- Sys.time()
-numberSims <- 100            # Start with 10. then try 100 or 1000.
-k <- 2                       # Number of ancestries.
-population1 <- "CEU_MAF"     # Name your ancestries
-population2 <- "afr_MAF"     # 
-popfrac1 <- runif(1, 0, 0.5) # Uniform random from user chosen interval.
-popfrac2 <- 1 - popfrac1     # Uniform random from user chosen interval.
-guess1 <- .25                # User chosen starting value.
-guess2 <- 1 - guess1         # User chosen sarting value.
-output <- c()                # initialize an empty output matrix.
-teamMember <- "your_name"    # Put your name here.
-seed <- 1                    # get this from snpsampgen.R. the same seed is used for both python and r versions of SLSQP.
+teamMember <- "your_name"              # Put your name here.
+numberSims <- 5                        # Start with 10. then try 100 or 1000.
+k <-5                                  # Keep at 5. This number is input for simulating the sampling population.
+numAnc <- 2                            # Set by the user. This number is only for recording in the spreadsheet.
+
+guess1 <- 1/k                           #              
+
+output <- c()                          # Initialize an empty output matrix.
 
 for (i in 1:numberSims){
-    A <- snpsampgen(k, population1 , population2, popfrac1, popfrac2)  # Simulating a sample pop with new SNPs.
+    
+    seed.unif <- 1 # save the seed used in the uniform draw     # Seed for SNP sampling and uniform draws.
+    eurfrac <- runif(1, 0.01, 0.05)    # Uniform random from user chosen interval.
+    afrfrac <- 1 - eurfrac             
+    easfrac <- 0
+    sasfrac <- 0
+    namfrac <- 0
+    
+    Seed.A <- 1 # Save the seed used in the sample simulation.    
+    A <- snpsampgen(k, "eur_MAF" , "afr_MAF", "eas_MAF", "sas_MAF", "nam_MAF", eurfrac, afrfrac, easfrac, sasfrac, namfrac)    
+    # Simulating a sample pop with new SNPs.
     
     # Calling the SLSQPmixtures function from SLSQPmixturesR.R
-    Python_ <- print(SLSQPmixtures(A, k))                              # Pi-values, iters, time.
+    Python_ <- print(SLSQPmixtures(A, k))                              # stores pi-values, iters, time.
     
     # Calling the HA function from HA_script.py                      
-    af <- cbind(A$AF)                                                  # Total allele frequency
-    A <- cbind(A$CEU_MAF, A$afr_MAF)                                   # CHANGEME: cbind(A$afr_MAF, A$CEU_MAF) etc
-    guess <- rbind(guess1, guess2)                                     # kx1 vector of starting guesses.
-    HA_ <- print(HA(A, af, guess))                                     # Pi-values, iters, time.
+    af <- cbind(A$AF)                                                  # Total allele frequency vector
+    A <- cbind(A$eur_MAF, A$afr_MAF, A$eas_MAF, A$sas_MAF, A$nam_MAF)  # CHANGEME: cbind(A$afr_MAF, A$CEU_MAF) etc
     
-    output <- rbind(output, c(HA_[1], HA_[2], HA_[3], Python_[1], Python_[2], Python_[3], seed, population1, popfrac1, guess1, population2, popfrac2, guess2, teamMember))
+    guess <- rbind(guess1, guess1, guess1, guess1, guess1)             # kx1 vector of starting guesses.
+    HA_ <- print(HA(A, af, guess))                                     # Stores pi-values, iters, time.
+    
+    output <- rbind(output, c(HA_[1], HA_[2], HA_[3], HA_[4], HA_[5], HA_[6], HA_[7], Python_[1], Python_[2], Python_[3], Python_[4], Python_[5], Python_[6], Python_[7], seed.unif, seed.A, teamMember, eurfrac, afrfrac, easfrac, sasfrac, namfrac))
     print(i)
+    print(Sys.time() - START)
 }
+colnames(output) <- c("R.eur", "R.afr", "R.eas", "R.sas", "R.nam",  "R.iters", "R.time","Python.eur", "Python.afr", "Python.eas", "Python.sas", "Python.nam", "Python.iters", "Python.time", "Seed.uniform", "Seed.A", "Team.memeber", "True.eur", "True.afr", "True.eas", "True.sas", "True.nam")
 
 END <- Sys.time()
 END - START
